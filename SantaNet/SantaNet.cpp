c2fcb344e6a928d66a2e88b914b08a6396dfbbe9
Santa net <horoseeb@gmail.com>
2021-03-14 07:17:40 +0600

    update

--- SantaNet/SantaNet.cpp
+++ SantaNet/SantaNet.cpp
@@ -1,22 +1,11 @@
 ﻿#include <iostream>
 int main()
 {
-	uint16_t arr[20];
-	uint16_t count = 0;
-	for (uint16_t i = 0; i < 19; i++)
+	
+	uint16_t n;
+	for (int16_t i = 0; i < n; i++)
 	{
-		arr[i] = rand() % 20000 - 10000;
-		for (uint16_t i = 0; i < 18; i++)
-		{
-			for (uint16_t j = 1; j < 19; j++) {
-				int temp = arr[i] + arr[j];
-				if (temp % 2 == 0 && temp % 4 != 0)
-				{
-					count++;
-				}
-			}
-		}
+
 	}
-	std::cout << count << std::endl;
 }
 
